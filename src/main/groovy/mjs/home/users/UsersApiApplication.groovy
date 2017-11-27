fe1a36ef34963d3e49d91623d4e696702ad44bc7
Michael Shoemake <atlantatechie@gmail.com>
2017-11-27 11:50:45 -0500

    Created HelloWorld version of UsersApi using SpringBoot (includes ZooKeeper and Mongo integrations).

---
+++ src/main/groovy/mjs/home/users/UsersApiApplication.groovy
@@ -0,0 +1,12 @@
+package mjs.home.users
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UsersApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run UsersApiApplication, args
+	}
+}
